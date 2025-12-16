625986a2ef207448327de64505fe05b5365de6c0
Rishabh Aneja <rishabh.aneja@gmail.com>
2025-12-16 21:54:30 +1100

    noneed

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ st.markdown("""
                 font-size: 1rem !important;
                 margin-top: 0px !important;
                 margin-bottom: 0px !important;
+                margin-left: 5px !important;
             }
             h4 {
                 font-size: 1rem !important;
